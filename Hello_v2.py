2b1ea382c234ab60522fb25b844aa56108693a53
SomeStarman2050 <ta3520968@gmail.com>
2025-05-16 18:28:55 -0500

    Add files via upload

--- Hello_v2.py
+++ Hello_v2.py
@@ -7,6 +7,15 @@ app.config['DEBUG'] = True
 def hello():
    message = "Hello, Flask!"
    return message
+@app.route('/thanks')
+def thanks():
+   person = "Bob"
+   action = "dancing"
+   object= "shoes"
+   thing= "dancer"
+   closer= "thanks"
+   writer= "Richard"
+   return render_template("tynote.html", name = person, gift=object, verb=action, noun=thing, closing_word=closer, author=writer)
 @app.route('/form')
 def form():
    return render_template("favorite_form.html")
